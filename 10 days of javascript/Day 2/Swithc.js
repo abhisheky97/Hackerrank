7fe9ffb234df12247a987b6d1527f97e9eab6033
Abhishek Yadav <ayig2302@gmail.com>
2020-12-26 23:02:12 +0530

    Javascript

---
+++ 10 days of javascript/Day 2/Swithc.js
@@ -0,0 +1,76 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    var set1 = new Set("a","e","i","o","u");
+    var set2 = new Set("b","c","d","f","g")
+    var set3 = new Set("h","j","k","l","m")
+    var ch = s[0];
+    var num = 0;
+    if(set1.has(ch))
+    {
+        num = 1;
+    }
+    if(set2.has(ch))
+    {
+        num = 2;
+    }
+    if(set3.has(ch))
+    {
+        num = 3;
+    }
+    switch (num)
+    {
+        case 1:
+        {
+            letter = "A";
+            break;
+        }
+        case 2:
+        {
+            letter = "B";
+            break;
+        }
+        case 3:
+        {
+            letter = "C";
+            break;
+        }
+        default:
+        {
+            letter = "D";
+        }
+    }
+    
+    return letter;
+}
+
+
+function main() {
+    const s = readLine();
+    
+    console.log(getLetter(s));
+}
