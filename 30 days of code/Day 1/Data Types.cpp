38b304ae31bb6886abe9d062f550aa5ce6a4f786
Abhishek Yadav <ayig2302@gmail.com>
2020-12-27 11:48:27 +0530

    30 days of code

---
+++ 30 days of code/Day 1/Data Types.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    
+    // Declare second integer, double, and String variables.
+    int j;
+    double e;
+    string t;
+    // Read and save an integer, double, and String to your variables.
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    cin >> j >> e;
+    getline(cin >> ws, t);
+    
+    // Print the sum of both integer variables on a new line.
+    cout << i+j << "\n";
+    // Print the sum of the double variables on a new line.
+    cout << fixed << setprecision(1) << d+e << "\n";
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    cout << s+t;
+    return 0;
+}
