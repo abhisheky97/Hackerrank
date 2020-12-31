2bc6ae32ac25dd999f2eaa7759a3cf4ea997f3fb
Abhishek Yadav <ayig2302@gmail.com>
2020-12-31 22:18:43 +0530

    30 Days of Code

---
+++ 30 days of code/Day 3/Intro to Conditional statements.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if(N%2==0)
+    {
+        if(6<=N && N<=20)
+        {
+            cout << "Weird";
+        }
+        else
+        {
+            cout << "Not Weird";
+        }
+    }
+    else
+    {
+        cout << "Weird";
+    }
+    return 0;
+}
