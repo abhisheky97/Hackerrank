35b56064350eefc9931e44186353119ffc76365e
Abhishek Yadav <ayig2302@gmail.com>
2021-01-05 20:28:35 +0530

    30 days of Code

---
+++ 30 days of code/Day 10/Binary Numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int count = 0;
+    int max = 0;
+    while (n > 0) {
+        if (n % 2 == 1) {
+            count++;
+            if (count > max) max = count;
+        }
+         else {
+            count = 0;
+        }
+        n = n/2;
+    }
+    cout << max;
+    return 0;
+}
