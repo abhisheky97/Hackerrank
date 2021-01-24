2bb5a4701066121d4defb8c659557ef2d660872c
Abhishek Yadav <ayig2302@gmail.com>
2021-01-24 23:19:31 +0530

    30 days of code

---
+++ 30 days of code/Day 29/Bitwise And.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        int max = 0;
+        for (int i = 2; i < n; ++i)
+        {
+            for (int j = i + 1; j <= n; ++j)
+            {
+                int ij = i & j;
+                if (ij < k && ij > max) max = ij;
+            }
+        }
+
+        cout << max << endl;
+    }
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
